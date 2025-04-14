1707348d4782e4c91a36f5f7667b1caf71735898
saryu <saryum46@gmail,com>
2025-04-14 20:08:18 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express') ;
+const app = express() ;
+app.get('/' , (req , res) =>{
+  res.send("Hello from the server !")
+})
+app.get('/home' ,(req , res) =>{
+  res.send("Hello from the Home !")
+})
+app.get('/about' ,(req , res) =>{
+  res.send("Hello from the About section !")
+})
+app.listen(5000 , () =>{
+ console.log("Server is running on port 5000...")
+})
