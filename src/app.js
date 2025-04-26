14e3973e099cdc4f8ae9d706eadd18a2b0dd5874
saryu <saryum46@gmail,com>
2025-04-26 16:24:01 +0530

    exploring get , push and delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express') ;
 const app = express() ;
-app.get('/' , (req , res) =>{
-  res.send("Hello from the server !")
-})
-app.get('/home' ,(req , res) =>{
-  res.send("Hello from the Home !")
-})
+// app.get('/' , (req , res) =>{
+//   res.send("Hello from the server !")
+// })
+// app.get('/home' ,(req , res) =>{
+//   res.send("Hello from the Home !")
+// })
 app.get('/about' ,(req , res) =>{
-  res.send("Hello from the About section !")
+  res.send({firstName : "Saryu" , lastName : "Malik"})
+})
+
+app.post('/about' , (req , res) =>{
+  res.send("Data has been posted successfully") ;
+})
+
+app.delete('/about' , (req , res)=> {
+  res.send("Data has been deleted successfully") ; 
 })
 app.listen(5000 , () =>{
  console.log("Server is running on port 5000...")
