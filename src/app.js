982db50dbd519d1ed641bdfba6a1bbdba6f4a0d0
saryu <saryum46@gmail,com>
2025-05-14 15:30:55 +0530

    Databse Introduction

--- src/app.js
+++ src/app.js
@@ -1,20 +1,31 @@
 const express = require('express') ;
 const app = express() ;
+const connectDB = require('./config/database');
 const {adminauth} = require("./middlewares/auth")
+const  User = require('./models/user') ;
 
-app.use('/admin' , adminauth) ;
+app.post('/signup' , async (req , res) =>{
+  const user = new User ({
+    firstName : "Aayush" ,
+    lastName  : "Hans" ,
+    emailId   :  "Hans@gmail.com" , 
+    password : "Mummy@123"
+  }) ; 
 
-app.get('/userentry' , (req , res) =>{
-  res.send("Data successfully entered by the user") ;
-})
-app.get('/admin/getdata' , (req , res) =>{
-  res.send("Data fetched successfully") ;
+  await user.save() ; 
+  res.send ("User created successfully") ; 
 })
 
-app.get('admin/deletedata' , (req , res) =>{  
-  res.send("Data deleted successfully") ;
-})
 
-app.listen(5000 , ()=>{
-  console.log("Server is running on port 5000") ;
-})
+
+connectDB()
+  .then(() => {
+    console.log("Database Established successfully");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.log("Database cannot be connected", err);
+  });
+
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const connectDB = async()=>{
+  await mongoose.connect ("mongodb+srv://saryu:Saryu%40264@namastenode.xlozxuu.mongodb.net/devTinder")
+}
+
+
+module.exports = connectDB ;
